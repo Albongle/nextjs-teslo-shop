5a52ed0ca4f1eacca3180d2212e012b36e9b1b98
Alejandro Bongioanni <ANBongioanni@teco.com.ar>
2024-07-23 13:21:53 -0300

    chore: first commit

---
+++ src/app/(shop)/orders/[id]/page.tsx
@@ -0,0 +1,7 @@
+export default function OrdersWithIdPage() {
+  return (
+    <div>
+      <h1>Orders page with id</h1>
+    </div>
+  );
+}
---
+++ src/app/auth/new-account/page.tsx
@@ -0,0 +1,7 @@
+export default function NewAccount() {
+  return (
+    <main className="">
+      <h1>cuenta nueva</h1>
+    </main>
+  );
+}
